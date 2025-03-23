232dcb9364881fda736010e7ee2f42ba8cce71a7
web3nomad.eth <me@web3nomad.eth>
2025-03-23 13:33:50 +0800

    config allowed origins

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,11 @@ const nextConfig: NextConfig = {
   experimental: {
     // see https://nextjs.org/docs/app/api-reference/functions/forbidden
     authInterrupts: true,
+    serverActions: {
+      allowedOrigins: process.env.SERVER_ACTIONS_ALLOWED_ORIGINS
+        ? process.env.SERVER_ACTIONS_ALLOWED_ORIGINS.split(",")
+        : [],
+    },
   },
 };
 
