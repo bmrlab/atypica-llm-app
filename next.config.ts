048cb469e3ea04486b84be4feadc696fc06f7fcb
web3nomad.eth <me@web3nomad.eth>
2025-03-23 14:25:04 +0800

    remove allowed_origin config

--- next.config.ts
+++ next.config.ts
@@ -17,11 +17,12 @@ const nextConfig: NextConfig = {
   experimental: {
     // see https://nextjs.org/docs/app/api-reference/functions/forbidden
     authInterrupts: true,
-    serverActions: {
-      allowedOrigins: process.env.SERVER_ACTIONS_ALLOWED_ORIGINS
-        ? process.env.SERVER_ACTIONS_ALLOWED_ORIGINS.split(",")
-        : [],
-    },
+    // 这个暂时不需要，通过 proxy 改写了 host 和 origin
+    // serverActions: {
+    //   allowedOrigins: process.env.SERVER_ACTIONS_ALLOWED_ORIGINS
+    //     ? process.env.SERVER_ACTIONS_ALLOWED_ORIGINS.split(",")
+    //     : [],
+    // },
   },
 };
 
