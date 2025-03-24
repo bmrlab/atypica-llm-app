3dd11b7d198a2b8aefc6d74aaad5b3f52a1bf2eb
web3nomad.eth <me@web3nomad.eth>
2025-03-24 23:22:41 +0800

    fix fallback tool result style

--- src/app/study/ToolConsole/ToolConsole.tsx
+++ src/app/study/ToolConsole/ToolConsole.tsx
@@ -10,31 +10,31 @@ import ReasoningThinking from "./tools/ReasoningThinking";
 import ScoutTaskChat from "./tools/ScoutTaskChat";
 
 const FallbackToolDisplay = ({ toolInvocation }: { toolInvocation: ToolInvocation }) => {
-  const { toolName, args } = toolInvocation;
   return (
     <pre
       className={cn(
         "text-xs whitespace-pre-wrap bg-gray-50 border border-gray-100 rounded-lg p-2 font-mono",
       )}
     >
-      <div className="ml-2 mt-1 mb-2 font-bold">{toolName} exec args</div>
+      <div className="ml-1 my-2 font-bold">exec {toolInvocation.toolName}</div>
+      <div className="ml-1 mt-1 mb-1 text-gray-500">&gt;_ args</div>
       <table className="text-left">
         <tbody>
-          {Object.entries(args).map(([key, value]) => (
+          {Object.entries(toolInvocation.args).map(([key, value]) => (
             <tr key={key}>
-              <td className="p-2 align-top">{key}:</td>
-              <td className="p-2 whitespace-pre-wrap">
+              <td className="p-1 align-top">{key}:</td>
+              <td className="p-1 whitespace-pre-wrap">
                 {typeof value === "object" ? JSON.stringify(value, null, 2) : value?.toString()}
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-      <div className="ml-2 mt-4 mb-2 font-bold">result</div>
+      <div className="ml-1 mt-2 mb-2 text-gray-500">&gt;_ result</div>
       {toolInvocation.state === "result" ? (
-        <div className="text-xs whitespace-pre-wrap p-2">{toolInvocation.result.plainText}</div>
+        <div className="text-xs whitespace-pre-wrap p-1">{toolInvocation.result.plainText}</div>
       ) : (
-        <div className="p-2">
+        <div className="p-1">
           <LoaderIcon className="animate-spin" size={16} />
         </div>
       )}
